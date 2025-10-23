236018af95091d45ad8e934c79a41d8695a17b1d
Danielc794 <danielc794@nycstudents.net>
2025-10-23 14:25:28 +0000

    asdasd

--- VR_Unit2/VRPractical2_3/house.js
+++ VR_Unit2/VRPractical2_3/house.js
@@ -1,5 +1,23 @@
 class house{
-  constructor(x,y,z){
+  constructor(x,z){
     this.obj = document.createElement("a-entity");
 
     let base = document.createElement("a-box");
+    base.setAttribute("color","beige");
+    base.setAttribute("depth","2");
+    base.setAttribute("height","1");
+    base.setAttribute("width","2");
+    base.setAttribute("position","0 0.5 0");
+    this.obj.append( base );
+
+    let roof = document.createElement("a-cone");
+    roof.setAttribute("color","red");
+    roof.setAttribute("height","1");
+    roof.setAttribute("radius-bottom","1.5");
+    roof.setAttribute("position","0 1.5 0");
+    this.obj.append( roof );
+
+    this.obj.setAttribute("position",{x:x, y:0.5, z:z});
+    scene.append( this.obj )
+  }
+}
