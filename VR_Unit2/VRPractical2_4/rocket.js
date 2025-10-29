5bab9d2103231a89a5f8805bdf2054695d02661c
Danielc794 <danielc794@nycstudents.net>
2025-10-29 14:34:50 +0000

    asd

---
+++ VR_Unit2/VRPractical2_4/rocket.js
@@ -0,0 +1,24 @@
+class Rocket{
+  constructor(x,y,z){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.a = 0;
+    this.da = 0.001;
+
+    this.obj = document.createElement("a-entity");
+    
+    let body = document.createElement("a-cylinder");
+    body.setAttribute("position", `${x} ${y} ${z}`);
+    body.setAttribute("radius", "0.1");
+    body.setAttribute("height", "1");
+    body.setAttribute("color", "#FF0000");
+    this.obj.appendChild(body);
+  }
+
+    launch(){
+    this.a += this.da;
+    this.obj.setAttribute("position", {x:0, y:this.a, z:0});
+    }
+
+}
--- VR_Unit2/VRPractical2_4/script.js
+++ VR_Unit2/VRPractical2_4/script.js
@@ -3,13 +3,19 @@ let scene;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); 
-
+  for(let i=0; i<100; i++){
+		let rocket = new Rocket(rnd(-50,50), rnd(-1,-5), rnd(-50,50));
+		scene.appendChild(rocket.obj);
+	}
 
   loop();
 })
 
 function loop(){
 
-  
+  for(let rocket of rockets){
+    rocket.launch();
+  }
+
   window.requestAnimationFrame( loop );
 }
